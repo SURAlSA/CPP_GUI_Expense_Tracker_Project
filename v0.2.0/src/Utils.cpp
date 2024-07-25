58c6e6cb38143e90dd2126a1f946d496272c829e
Zane <145461046+SURAlSA@users.noreply.github.com>
2024-07-25 13:03:08 +1000

    Update Utils.cpp

--- v0.2.0/src/Utils.cpp
+++ v0.2.0/src/Utils.cpp
@@ -1,5 +1,6 @@
 #include "Utils.h"
 
-bool isPointInsideRect(int x, int y, SDL_Rect rect) {
+bool isPointInsideRect(int x, int y, SDL_Rect rect) 
+{
     return (x >= rect.x && x <= rect.x + rect.w && y >= rect.y && y <= rect.y + rect.h); // returns true if within a specified icon.
 }
